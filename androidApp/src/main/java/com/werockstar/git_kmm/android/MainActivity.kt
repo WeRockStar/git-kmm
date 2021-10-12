e630daaf6b4e598761e011dcc3ac53743fc2d890
WeRockStar <kotchaphan.m@gmail.com>
2021-10-12 22:01:40 +0700

    Add simple composable text

--- androidApp/src/main/java/com/werockstar/git_kmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/werockstar/git_kmm/android/MainActivity.kt
@@ -3,10 +3,28 @@ package com.werockstar.git_kmm.android
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.werockstar.git_kmm.Greeting
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            GreetingText()
+        }
     }
+}
+
+@Composable
+fun GreetingText() {
+    val greeting = Greeting().greeting()
+    Text(text = greeting)
+}
+
+@Preview
+@Composable
+fun GreetingPreview() {
+    GreetingText()
 }
