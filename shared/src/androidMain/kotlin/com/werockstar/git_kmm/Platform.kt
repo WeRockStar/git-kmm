dc676a536dd600e337ca5a31935f1bd2b86dd7a0
WeRockStar <kotchaphan.m@gmail.com>
2021-10-13 15:14:09 +0700

    Remove unused code

--- shared/src/androidMain/kotlin/com/werockstar/git_kmm/Platform.kt
+++
@@ -1,5 +0,0 @@
-package com.werockstar.git_kmm
-
-actual class Platform actual constructor() {
-    actual val platform: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
--- shared/src/commonMain/kotlin/com/werockstar/git_kmm/Platform.kt
+++
@@ -1,1 +0,0 @@
-package com.werockstar.git_kmm
