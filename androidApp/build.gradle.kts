1e8a959834fdcbbb576161c9ff4a8ba11bbd2b37
WeRockStar <kotchaphan.m@gmail.com>
2021-12-06 22:46:40 +0700

    Upgrade libraries

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 dependencies {
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
+    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
 
     implementation(Compose.runtime)
     implementation(Compose.runtimeLiveData)
