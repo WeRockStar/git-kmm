55ed6501133d94a27dd473968bdd2357fb3b9155
WeRockStar <kotchaphan.m@gmail.com>
2022-10-30 19:48:18 +0700

    Fix composable to support padding values from Scaffold

--- androidApp/src/main/java/com/werockstar/git_kmm/android/ui/GitUserScreen.kt
+++ androidApp/src/main/java/com/werockstar/git_kmm/android/ui/GitUserScreen.kt
@@ -1,23 +1,18 @@
 package com.werockstar.git_kmm.android.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.werockstar.git_kmm.android.ui.composable.GitUser
+import com.werockstar.git_kmm.ui.GitUser
 
 @Composable
-fun GitUserScreen(viewModel: GitUserViewModel = viewModel()) {
-    val users = viewModel.users.observeAsState(emptyList())
-    LaunchedEffect(Unit) {
-        viewModel.fetchUsers()
-    }
-    LazyColumn(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-        items(users.value) {
+fun GitUserScreen(paddingValues: PaddingValues, users: List<GitUser>, ) {
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(4.dp), contentPadding = paddingValues) {
+        items(users) {
             GitUser(user = it)
         }
     }
