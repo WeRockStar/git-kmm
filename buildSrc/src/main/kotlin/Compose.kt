3ae009ac7a8de01ad57262fca48f9beaca1dd22a
WeRockStar <kotchaphan.m@gmail.com>
2023-03-27 19:16:56 +0700

    Upgrade jetpack compose

--- buildSrc/src/main/kotlin/Compose.kt
+++ buildSrc/src/main/kotlin/Compose.kt
@@ -1,6 +1,6 @@
 object Compose {
-    const val composeVersion = "1.3.1"
-    const val composeCompilerVersion = "1.4.2"
+    const val composeVersion = "1.4.0"
+    const val composeCompilerVersion = "1.4.4"
     const val runtime = "androidx.compose.runtime:runtime:${composeVersion}"
     const val runtimeLiveData = "androidx.compose.runtime:runtime-livedata:${composeVersion}"
     const val ui = "androidx.compose.ui:ui:${composeVersion}"
@@ -16,7 +16,7 @@ object Compose {
     const val navigation = "androidx.navigation:navigation-compose:${composeNavigationVersion}"
 
     const val coil = "io.coil-kt:coil-compose:2.2.2"
-    const val accompanist = "com.google.accompanist:accompanist-placeholder:0.28.0"
+    const val accompanist = "com.google.accompanist:accompanist-placeholder:0.30.0"
 
     const val composeJUnitUITest = "androidx.compose.ui:ui-test-junit4:${composeVersion}"
     const val composeUITestManifest = "androidx.compose.ui:ui-test-manifest:${composeVersion}"
