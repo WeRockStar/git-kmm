717184b3e8a317170a0b2f113a9563fd46a2fa6a
WeRockStar <kotchaphan.m@gmail.com>
2023-05-18 20:59:46 +0700

    Upgrade how to define package name according to newer gradle

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -75,8 +75,8 @@ android {
         compose = true
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
         jvmTarget = "1.8"
@@ -85,9 +85,10 @@ android {
         kotlinCompilerExtensionVersion = Compose.composeCompilerVersion
     }
     namespace = AndroidApp.applicationId
+    testNamespace = "com.werockstar.gitkmm.android.test"
 }
 
 shot {
-    tolerance =  0.15
+    tolerance = 0.15
     showOnlyFailingTestsInReports = true
 }
